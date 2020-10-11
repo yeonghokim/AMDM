24b773856cdef7220b25b6fa0ed4480179dbce96
yeongho kim <yeongho.kim2000@gmail.com>
2020-10-11 11:21:26 +0000

    android unzip commit

---
+++ AMDM_Android/app/src/main/java/com/chunma/amdm/TurnOnReceiver.java
@@ -0,0 +1,26 @@
+package com.chunma.amdm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class TurnOnReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        String action = intent.getAction();
+
+        switch (action){
+            case "android.intent.action.BOOT_COMPLETED":
+                String str = "브로드캐스트가 실행되었습니다.";
+                Toast t1 = Toast.makeText(context,str,Toast.LENGTH_SHORT);
+                t1.show();
+                break;
+        }
+
+
+
+    }
+}

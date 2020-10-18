8b34c4f9a4309c9f77b8e001091bd373365fcf27
yeongho kim <yeongho.kim2000@gmail.com>
2020-10-18 07:45:56 +0000

    android commit

--- AMDM_Android/app/src/main/java/com/chunma/amdm/mainfragment/MainLockFragment.java
+++ AMDM_Android/app/src/main/java/com/chunma/amdm/mainfragment/MainLockFragment.java
@@ -29,16 +29,7 @@ public class MainLockFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        Button button = (Button)getActivity().findViewById(R.id.mainlockbutton);
 
-        final Context context =getContext();
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context,"make toast", Toast.LENGTH_LONG);
-
-            }
-        });
         return inflater.inflate(R.layout.fragment_main_lock, container, false);
     }
 }
